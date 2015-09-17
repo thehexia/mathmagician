1ec50df921ae3edd5481b482cd1db85841bbfe5a
Hoang Nguyen <hvn1@hvn1-Inspiron-N5040.(none)>
2015-09-17 17:04:45 -0400

    fixed issue with kind() check on null token

--- parse-expr.cpp
+++ parse-expr.cpp
@@ -9,26 +9,6 @@
 namespace math
 {
 
-
-// int
-// precedence(Token_kind k)
-// {
-//   switch (k) {
-//   case integer_tok:
-//     return 0; 
-//   case plus_tok:
-//   case minus_tok:
-//     return 2;
-//   case star_tok: 
-//   case fslash_tok:
-//   case mod_tok:
-//     return 3;
-//   default:
-//     return -1;
-//   }
-// }
-
-
 bool
 is_binary_op(Token_kind k)
 {
@@ -72,93 +52,6 @@ is_mult_op(Token_kind k)
 }
 
 
-// // stolen straight off wikipedia
-// // parse_expression_1 (lhs, min_precedence)
-// //     lookahead := peek next token
-// //     while lookahead is a binary operator whose precedence is >= min_precedence
-// //         op := lookahead
-// //         advance to next token
-// //         rhs := parse_primary ()
-// //         lookahead := peek next token
-// //         while lookahead is a binary operator whose precedence is greater
-// //                  than op's, or a right-associative operator
-// //                  whose precedence is equal to op's
-// //             rhs := parse_expression_1 (rhs, lookahead's precedence)
-// //             lookahead := peek next token
-// //         lhs := the result of applying op with operands lhs and rhs
-// //     return lhs
-// Expr*
-// parse_left_infix_expr(Parser& p, Token_stream& ts, Expr* e1, int min_prec)
-// {
-//   // look at the next operator
-//   // if it exists
-//   while(Token const* l = ts.next()) {
-//     // while l is a binary operator and its precedence >= min prec
-//     if (is_binary_op(l->kind()) && precedence(l->kind()) >= min_prec) {
-//       // set the operator
-//       Token const* op = l;
-//       // advance to the next
-//       ts.advance();
-//       // if we can parse another primary expression
-//       if (Expr* e2 = parse_primary_expr(p, ts)) {
-//         l = ts.next();
-//         while (l) {
-//           if (is_binary_op(op->kind()) && precedence(op->kind()) > min_prec) {
-//             e2 = parse_left_infix_expr(p, ts, e2, precedence(l->kind()));
-//             l = ts.advance();
-//           }
-//         }
-//         e1 = p.on_arithmetic(op, e1, e2);
-//       }
-//       else
-//         std::cout << "Error: expected second operand \n"; 
-//     }
-//   }
-
-//   return e1;
-// }
-
-
-// Expr*
-// parse_paren_enclosed(Parser& p, Token_stream& ts)
-// {
-//   return nullptr;
-// }
-
-
-// Expr* 
-// parse_integer(Parser& p, Token_stream& ts)
-// {
-//   if (Token const* tok = ts.expect(integer_tok))
-//     return p.on_integer(tok);
-//   return nullptr;
-// }
-
-
-// Expr*
-// parse_unary(Parser& p, Token_stream& ts)
-// {
-//   return nullptr;
-// }
-
-
-// Expr*
-// parse_primary_expr(Parser& p, Token_stream& ts)
-// {
-//   if (!ts.next())
-//     return nullptr;
-
-//   switch (ts.next()->kind()) {
-//     case integer_tok: return parse_integer(p, ts);
-//     case plus_tok:
-//     case minus_tok:
-//       return parse_unary(p, ts);
-//     default:
-//       return nullptr;
-//   }
-// }
-
-
 Expr*
 parse_integer(Parser& p, Token_stream& ts)
 {
@@ -197,11 +90,13 @@ parse_paren_enclosed(Parser& p, Token_stream& ts)
 Expr*
 parse_term(Parser& p, Token_stream& ts)
 {
-  switch (ts.next()->kind()) {
-    case integer_tok: return parse_integer(p, ts);
-    case lparen_tok: return parse_paren_enclosed(p, ts);
-    default:
-      return nullptr;
+  if (ts.next()) {
+    switch (ts.next()->kind()) {
+      case integer_tok: return parse_integer(p, ts);
+      case lparen_tok: return parse_paren_enclosed(p, ts);
+      default:
+        return nullptr;
+    }
   }
 }
 
@@ -287,7 +182,7 @@ parse_additive_expr(Parser& p, Token_stream& ts, Token const* tok, Expr* e1)
   error("Expected expression after: ");
   print(e1);
   print(tok);
-  print(ts.next());
+  
   return nullptr;
 }
 
