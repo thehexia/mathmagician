14e51e2d4e2787b48f58566d51041ad479c80daf
Hoang Nguyen <hvn1@hvn1-Inspiron-N5040.(none)>
2015-09-22 12:00:40 -0400

    moving everything into calc folder

---
+++ calc/build/test/CMakeFiles/test-lex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test-lex.dir/test-lex.cpp.o"
+  "test-lex.pdb"
+  "test-lex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test-lex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ calc/build/test/CMakeFiles/test-postfix.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test-postfix.dir/test-postfix.cpp.o"
+  "test-postfix.pdb"
+  "test-postfix"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test-postfix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
